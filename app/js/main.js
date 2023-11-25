bf2cc7cdcd0f43c121202257bc1bf9ad6ce0db66
Aneta Nosareva <admospk@mail.ru>
2023-11-25 20:01:17 +0500

    Calendar process

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
-
 $(function () {
-
+    const picker = new easepick.create({
+        element: document.getElementById('checkin'),
+        css: [
+            "https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css"
+        ],
+        zIndex: 3,
+        lang: "ru-RU",
+        format: "DD.MM.YYYY",        
+        RangePlugin: {
+            elementEnd: document.getElementById('checkout'),            
+        },
+        plugins: ['RangePlugin']
     })
+    });
